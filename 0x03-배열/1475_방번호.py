c4a19794aacd5ebd73977a88a3d866866484b8c6
vsuminv <tnals@naver.com>
2023-10-22 21:54:16 +0900

    1475 정답 코드

--- 0x03-배열/1475_방번호.py
+++ 0x03-배열/1475_방번호.py
@@ -1,6 +1,13 @@
 N = input()
-
+nums = [0] * 10
+cnt = 0
 for i in range(len(N)):
-    num = int(N[i])
-    print(num)
-# if 
+    num = (int(N[i]))
+    if num == 6 or num == 9:
+        if nums[6] <= nums[9]:
+            nums[6] += 1
+        else:
+            nums[9] += 1
+    else:
+        nums[num] += 1
+print(max(nums))
